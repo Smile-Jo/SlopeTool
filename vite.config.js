04b28622b49a4170fd7ff73f998baa439b464c64
Smile-Jo <rok519674272@gmail.com>
2025-07-09 13:53:45 +0900

    build 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './', // 상대 경로로 빌드하도록 설정
   publicDir: 'public',
   resolve: {
     alias: {
